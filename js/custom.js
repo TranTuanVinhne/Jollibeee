81242e202d35cc9020357f25ad4cf51a7f8ca9b7
TranTuanVinhne <151108941+TranTuanVinhne@users.noreply.github.com>
2023-12-01 17:46:52 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+$('#intro-chay').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+	autoplay:true,
+	autoplayTimeout:5000,
+	smartSpeed:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
